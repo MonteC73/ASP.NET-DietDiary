d5cd439499cca9508d789fb49c35d9eab8548dce
MonteC73 <jarekucinski@gmail.com>
2017-04-04 15:37:16 +0200

    Initial commit

---
+++ CountingKs/Global.asax.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json.Serialization;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace CountingKs
+{
+  // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+  // visit http://go.microsoft.com/?LinkId=9394801
+
+  public class MvcApplication : System.Web.HttpApplication
+  {
+    protected void Application_Start()
+    {
+      HttpConfiguration config = GlobalConfiguration.Configuration;
+      config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+      config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
+      config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling =
+          Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+      config.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+
+      AreaRegistration.RegisterAllAreas();
+
+      WebApiConfig.Register(GlobalConfiguration.Configuration);
+      FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+      RouteConfig.RegisterRoutes(RouteTable.Routes);
+      BundleConfig.RegisterBundles(BundleTable.Bundles);
+      AuthConfig.RegisterAuth();
+    }
+  }
+}
