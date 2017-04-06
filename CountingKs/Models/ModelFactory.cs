610fb077c47f4f741f1a1628a4f88503ae4498de
MonteC73 <jarekucinski@gmail.com>
2017-04-06 15:29:42 +0200

    Implement Post and Delete in DiariesController

--- CountingKs/Models/ModelFactory.cs
+++ CountingKs/Models/ModelFactory.cs
@@ -91,6 +91,33 @@ namespace CountingKs.Models
             }
         }
 
+        public Diary Parse(DiaryModel model)
+        {
+            try
+            {
+                var entity = new Diary();
+
+                if (!string.IsNullOrWhiteSpace(model.Url))
+                {
+                    var uri = new Uri(model.Url);
+                    entity.Id = int.Parse(uri.Segments.Last());
+                }
+
+                entity.CurrentDate = model.CurrentDate;
+
+                if (model.Entries != null)
+                {
+                    foreach (var entry in model.Entries) entity.Entries.Add(Parse(entry));
+                }
+
+                return entity;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
         public DiarySummaryModel CreateSummary(Diary diary)
         {
             return new DiarySummaryModel()
